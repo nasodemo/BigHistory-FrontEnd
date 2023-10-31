1fc5005382ad271c87e67a654de7e0e08f3dce6a
eduminsun <133388000+eduminsun@users.noreply.github.com>
2023-10-31 13:31:42 +0900

    Update Searchbar.tsx

--- src/app/(Searchbar)/Searchbar.tsx
+++ src/app/(Searchbar)/Searchbar.tsx
@@ -58,7 +58,7 @@ const SearchBar = () => {
       {showDropbox && (
         <div ref={dropdownRef} className={styles.dropbox}>
           <p style={{fontSize:'11px', color:'grey'}}>검증 완료된 예시 키워드가 표시됩니다.</p>
-          <Link href='/지구온난화' className={styles.linkStyle}>지구온난화</Link>
+          <Link href='/기후변화' className={styles.linkStyle}>기후변화</Link>
         </div>
       )}
       
